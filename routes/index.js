3f049792fa53d80d9e2ade4333d4c1c93a4e37d5
Muhammad Thoriq Az-Dzaky <102453829+Luckyboyyy09@users.noreply.github.com>
2023-06-13 10:34:49 +0800

    delete method getUsers

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { getUsers, Register, Login, Logout, allitems, populeritems, scanitem, resetPassword } from '../controllers/Users.js';
+import { Register, Login, Logout, allitems, populeritems, scanitem, resetPassword } from '../controllers/Users.js';
 import { verifyToken } from '../middleware/VerifyToken.js';
 import { refershToken } from '../controllers/RefreshToken.js';
 
 const router = express.Router();
 
-router.get('/users', verifyToken, getUsers);
 router.post('/register', Register);
 router.post('/login', Login);
 router.get('/fruits', allitems);
@@ -13,7 +12,7 @@ router.get('/fruits/popular', populeritems);
 router.get('/token', refershToken);
 router.get('/detection', scanitem)
 router.delete('/logout', Logout);
-router.post('resetPassword', resetPassword)
+router.post('/resetPassword', resetPassword)
 
 
 export default router;
